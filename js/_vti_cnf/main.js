49087a31f03ecdd51ac801c273c07e8a99c45558
demetyalmanozen <demetyalmann@gmail.com>
2023-10-17 21:11:59 -0700

    Remove template

--- js/_vti_cnf/main.js
+++
@@ -1,9 +0,0 @@
-vti_encoding:SR|utf8-nl
-vti_timelastmodified:TR|30 Apr 2018 10:38:10 -0000
-vti_extenderversion:SR|12.0.0.0
-vti_author:SR|DESKTOP-UUBFUT5\\Demet Yalman Ozen
-vti_modifiedby:SR|DESKTOP-UUBFUT5\\Demet Yalman Ozen
-vti_timecreated:TR|30 Apr 2018 10:38:10 -0000
-vti_cacheddtm:TX|30 Apr 2018 10:38:10 -0000
-vti_filesize:IR|6789
-vti_backlinkinfo:VX|index.html
