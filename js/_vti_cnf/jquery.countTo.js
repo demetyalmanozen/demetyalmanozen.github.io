320e9ce297d1d676c9b49bae0c4a604ffd332dc6
demetyalmanozen <demetyalmann@gmail.com>
2023-10-17 21:06:44 -0700

    Uploading the source files

---
+++ js/_vti_cnf/jquery.countTo.js
@@ -0,0 +1,9 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|15 Aug 2016 16:00:36 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_author:SR|DESKTOP-UUBFUT5\\Demet Yalman Ozen
+vti_modifiedby:SR|DESKTOP-UUBFUT5\\Demet Yalman Ozen
+vti_timecreated:TR|15 Aug 2016 16:00:36 -0000
+vti_cacheddtm:TX|15 Aug 2016 16:00:36 -0000
+vti_filesize:IR|3760
+vti_backlinkinfo:VX|index.html
---
+++ js/_vti_cnf/jquery.flexslider-min.js
@@ -0,0 +1,9 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|17 Nov 2015 10:33:30 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_author:SR|DESKTOP-UUBFUT5\\Demet Yalman Ozen
+vti_modifiedby:SR|DESKTOP-UUBFUT5\\Demet Yalman Ozen
+vti_timecreated:TR|17 Nov 2015 10:33:30 -0000
+vti_cacheddtm:TX|17 Nov 2015 10:33:30 -0000
+vti_filesize:IR|22342
+vti_backlinkinfo:VX|index.html
